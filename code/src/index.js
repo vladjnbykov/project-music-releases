e0ec2d3801b92ba30276ed9eb2892ba7aef72653
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-05-10 01:06:34 +0200

    response to comments, centering icons, fixing background, dependencies

--- code/src/index.js
+++ code/src/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
 import App from './App'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
